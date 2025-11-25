d07603e532692b2b0c6c8702a32e35399d6708f3
unknown <2201130010ew@cyber-u.ac.jp>
2025-11-25 18:14:08 +0900

    add dog api

--- dog/routes/notes.js
+++ dog/routes/notes.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://matsunami2:**********@test.zlsie9f.mongodb.net/?retryWrites=true&w=majority&appName=test";
+const uri = "mongodb+srv://ix8fate_db_user:kRoB2R7lnTYOPs3J@cluster0.asukfqn.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
@@ -14,7 +12,7 @@ const database = client.db('notes');
 const notes = database.collection('notes');
 
 // idが１のドキュメントを取得
-const query = { id: 2 };
+const query = { id: 1 };
 const note = await notes.findOne(query);
 
 res.json(note);
